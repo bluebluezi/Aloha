0e9956a1a0db882add4a9586f6dd305181dc7d51
bluebluezi <ng.johnnyzi@gmail.com>
2020-01-27 07:18:08 -0500

    fix: added media query for breakpoints and email form validation

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("working fine");
 
-  var elem = document.querySelector(".main-carousel");
-  var flkty = new Flickity(elem, {
-    // options
-    cellAlign: "left",
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity( elem, {
+    wrapAround: true,
+    adaptiveHeight:false,
+    cellAlign: 'left',
     contain: true
   });
-
+  
   // element argument can be a selector string
   //   for an individual element
-  var flkty = new Flickity(".main-carousel", {
+  var flkty = new Flickity( '.main-carousel', {
     // options
   });
 });
+
+
+
+const submitButton = document.getElementsByClassName("submit-button")[0];
+
+submitButton.addEventListener("click", function(){
+  const emailTextArea = document.getElementsByClassName("input-textbox")[0];
+  const result = validateEmail(emailTextArea.value);
+    if (!result){
+      alert("You have entered an invalid email address!");
+    }
+    else{
+      alert("Thank you for subscribing!");
+    }
+
+});
+
+
+
+
+/* source: https://tylermcginnis.com/validate-email-address-javascript/*/ 
+function validateEmail(mail) {
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    return (true)
+  }
+
+    return (false)
+}
